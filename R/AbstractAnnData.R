aa62d2525d5d76312c82157208807f94c56de112
Robrecht Cannoodt <rcannood@gmail.com>
2023-04-27 10:19:49 +0200

    rename BackedH5AD to HDF5AnnData

--- R/AbstractAnnData.R
+++ R/AbstractAnnData.R
@@ -3,7 +3,6 @@
 #' @description
 #' Abstract class representing an anndata object. Defines the interface.
 #' @importFrom R6 R6Class
-#' 
 AbstractAnnData <- R6::R6Class("AbstractAnnData",
   public = list(
 
--- R/HDF5AnnData.R
+++ R/HDF5AnnData.R
@@ -1,9 +1,9 @@
-#' @title BackedH5AD
+#' @title HDF5AnnData
 #'
 #' @description
 #' Representation of an anndata object, disk-backed.
 #' @importFrom R6 R6Class
-BackedH5AD <- R6Class("BackedH5AD",
+HDF5AnnData <- R6Class("HDF5AnnData",
     public = list(
       inherit = AbstractAnnData,
       h5object = NULL,
