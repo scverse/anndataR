0cc1484cb2a7b5ab57aa1b9ab46d467fb4292219
Robrecht Cannoodt <rcannood@gmail.com>
2023-04-27 17:48:43 +0200

    move validator functions

--- R/InMemoryAnnData.R
+++ R/InMemoryAnnData.R
@@ -11,7 +11,65 @@ InMemoryAnnData <- R6::R6Class("InMemoryAnnData",
     .obs = NULL,
     .var = NULL,
     .obs_names = NULL,
-    .var_names = NULL
+    .var_names = NULL,
+
+    #' @description validate a matrix (.X or .layers[...])
+    .validate_matrix = function(mat, obj_name) {
+      if (!is.null(mat)) {
+        if (nrow(mat) != nrow(self$obs))
+          stop("nrow(", obj_name, ") should be the same as nrow(obs)")
+        if (ncol(mat) != nrow(self$var))
+          stop("ncol(", obj_name, ") should be the same as nrow(var)")
+      
+        if (!is.null(rownames(mat))) {
+          warning("rownames(", obj_name, ") should be NULL, removing them from the matrix")
+          rownames(mat) <- NULL
+        }
+      
+        if (!is.null(colnames(mat))) {
+          warning("colnames(", obj_name, ") should be NULL, removing them from the matrix")
+          colnames(mat) <- NULL
+        }
+      }
+
+      mat
+    },
+
+    #' @description validate an obs data frame
+    .validate_obs = function(obs) {
+      if (is.null(obs)) stop("obs should be a data frame")
+      if (.row_names_info(obs) >= 0) {
+        warning("obs should not have any dimnames, removing them from the matrix")
+        rownames(obs) <- NULL
+      }
+      obs
+    },
+
+    #' @description validate a var data frame
+    .validate_var = function(var) {
+      if (is.null(var)) stop("var should be a data frame")
+      if (.row_names_info(var) >= 0) {
+        warning("var should not have any rownames, removing them from the matrix")
+        rownames(var) <- NULL
+      }
+      var
+    },
+
+    #' @description validate an obs_names vector
+    .validate_obs_names = function(obs_names) {
+      if (!is.null(obs_names)) {
+        if (length(obs_names) != nrow(self$obs)) stop("length(obs_names) should be the same as nrow(obs)")
+      }
+      obs_names
+    },
+
+    #' @description validate a var_names vector
+    .validate_var_names = function(var_names) {
+      if (!is.null(var_names)) {
+        if (length(var_names) != nrow(self$var)) stop("length(var_names) should be the same as nrow(var)")
+      }
+      var_names
+    }
   ),
   active = list(
     #' @field X The X slot
@@ -19,7 +77,7 @@ InMemoryAnnData <- R6::R6Class("InMemoryAnnData",
       if (missing(value)) {
         private$.X
       } else {
-        private$.X <- .inmemoryanndata_check_matrix(value, self$obs, self$var, "X")
+        private$.X <- self$.validate_matrix(value, "X")
       }
     },
     #' @field obs The obs slot
@@ -27,7 +85,7 @@ InMemoryAnnData <- R6::R6Class("InMemoryAnnData",
       if (missing(value)) {
         private$.obs
       } else {
-        private$.obs <- .inmemoryanndata_check_obs(value)
+        private$.obs <- self$.validate_obs(value)
       }
     },
     #' @field var The var slot
@@ -35,7 +93,7 @@ InMemoryAnnData <- R6::R6Class("InMemoryAnnData",
       if (missing(value)) {
         private$.var
       } else {
-        private$.var <- .inmemoryanndata_check_var(value)
+        private$.var <- self$.validate_var(value)
       }
     },
     #' @field obs_names The obs_names slot
@@ -43,7 +101,7 @@ InMemoryAnnData <- R6::R6Class("InMemoryAnnData",
       if (missing(value)) {
         private$.obs_names
       } else {
-        private$.obs_names <- .inmemoryanndata_check_obs_names(value)
+        private$.obs_names <- self$.validate_obs_names(value)
       }
     },
     #' @field var_names The var_names slot
@@ -51,7 +109,7 @@ InMemoryAnnData <- R6::R6Class("InMemoryAnnData",
       if (missing(value)) {
         private$.var_names
       } else {
-        private$.var_names <- .inmemoryanndata_check_var_names(value)
+        private$.var_names <- self$.validate_var_names(value)
       }
     }
   ),
@@ -65,70 +123,22 @@ InMemoryAnnData <- R6::R6Class("InMemoryAnnData",
     #' @param obs_names The obs_names slot
     #' @param var_names The var_names slot
     initialize = function(X = NULL, obs, var, obs_names = NULL, var_names = NULL) {
+      # check obs and var first
+      obs <- private$.validate_obs(obs)
+      var <- private$.validate_var(var)
+      private$.obs <- obs
+      private$.var <- var
+
       # check inputs
-      X <- .inmemoryanndata_check_matrix(X, obs, var, "X")
-      obs <- .inmemoryanndata_check_obs(obs)
-      var <- .inmemoryanndata_check_var(var)
-      obs_names <- .inmemoryanndata_check_obs_names(obs_names, obs)
-      var_names <- .inmemoryanndata_check_var_names(var_names, var)
+      X <- private$.validate_matrix(X, "X")
+      obs_names <- private$.validate_obs_names(obs_names)
+      var_names <- private$.validate_var_names(var_names)
 
       # store results
       private$.X <- X
-      private$.obs <- obs
-      private$.var <- var
       private$.obs_names <- obs_names
       private$.var_names <- var_names
     }
   )
 )
 
-.inmemoryanndata_check_matrix <- function(mat, obs, var, obj_name) {
-  if (!is.null(mat)) {
-    if (nrow(mat) != nrow(obs)) stop("nrow(", obj_name, ") should be the same as nrow(obs)")
-    if (ncol(mat) != nrow(var)) stop("ncol(", obj_name, ") should be the same as nrow(var)")
-  
-    if (!is.null(rownames(mat))) {
-      warning("rownames(", obj_name, ") should be NULL, removing them from the matrix")
-      rownames(mat) <- NULL
-    }
-  
-    if (!is.null(colnames(mat))) {
-      warning("colnames(", obj_name, ") should be NULL, removing them from the matrix")
-      colnames(mat) <- NULL
-    }
-  }
-
-  mat
-}
-
-.inmemoryanndata_check_obs <- function(obs) {
-  if (is.null(obs)) stop("obs should be a data frame")
-  if (.row_names_info(obs) >= 0) {
-    warning("obs should not have any dimnames, removing them from the matrix")
-    rownames(obs) <- NULL
-  }
-  obs
-}
-
-.inmemoryanndata_check_var <- function(var) {
-  if (is.null(var)) stop("var should be a data frame")
-  if (.row_names_info(var) >= 0) {
-    warning("var should not have any rownames, removing them from the matrix")
-    rownames(var) <- NULL
-  }
-  var
-}
-
-.inmemoryanndata_check_obs_names <- function(obs_names, obs) {
-  if (!is.null(obs_names)) {
-    if (length(obs_names) != nrow(obs)) stop("length(obs_names) should be the same as nrow(obs)")
-  }
-  obs_names
-}
-
-.inmemoryanndata_check_var_names <- function(var_names, var) {
-  if (!is.null(var_names)) {
-    if (length(var_names) != nrow(var)) stop("length(var_names) should be the same as nrow(var)")
-  }
-  var_names
-}
