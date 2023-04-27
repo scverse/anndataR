c522c4c363a4bbf709dcf112939847c9d7b53fd4
Robrecht Cannoodt <rcannood@gmail.com>
2023-04-27 11:13:44 +0200

    Add X, obs and var to the abstract interface
    Co-authored-by: Louise Deconinck <LouiseDck@users.noreply.github.com>
    Co-authored-by: Luke Zappia <lazappi@users.noreply.github.com>
    Co-authored-by: Martin Morgan <mtmorgan.bioc@gmail.com>

--- R/AbstractAnnData.R
+++ R/AbstractAnnData.R
@@ -1,6 +1,25 @@
+.abstract_function <- function() {
+  stop("This function is not implemented yet.")
+}
+
 #' @title AbstractAnnData
 #'
 #' @description
 #' Abstract class representing an anndata object. Defines the interface.
 #' @importFrom R6 R6Class
-AbstractAnnData <- R6::R6Class("AbstractAnnData")
+AbstractAnnData <- R6::R6Class("AbstractAnnData",
+  active = list(
+    #' @field X The X slot
+    X = function(value) {
+      .abstract_function()
+    },
+    #' @field obs The obs slot
+    obs = function(value) {
+      .abstract_function()
+    },
+    #' @field var The var slot
+    var = function(value) {
+      .abstract_function()
+    }
+  )
+)
