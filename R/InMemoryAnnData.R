0aaade4da42ae2c4cf747b087d73e5261b9d0f7b
Robrecht Cannoodt <rcannood@gmail.com>
2023-04-27 13:49:57 +0200

    add validators to the inmemory constructor

--- R/InMemoryAnnData.R
+++ R/InMemoryAnnData.R
@@ -41,10 +41,58 @@ InMemoryAnnData <- R6::R6Class("InMemoryAnnData",
     #' @description Creates a new instance of an in memory AnnData object.
     #' Inherits from AbstractAnnData
     #' 
-    #' @param X The count matrix
-    #' @param obs The associated observation metadata
-    #' @param var The associated variables metadata
-    initialize = function(X = NULL, obs = NULL, var = NULL) {
+    #' @param X A #observations × #variables data matrix. A view of the data is used if the data type matches, otherwise, a copy is made.
+    #' @param obs Key-indexed one-dimensional observations annotation of length #observations.
+    #' @param var Key-indexed one-dimensional variables annotation of length #variables.
+    #' @param shape Shape list (#observations, #variables). Can only be provided if `X` is `NULL`.
+    initialize = function(X = NULL, obs = NULL, var = NULL, shape = NULL) {
+      
+      # check nrow size
+      nrow <- nrow(X)
+      if (is.null(nrow)) nrow <- nrow(obs)
+      if (is.null(nrow) && !is.null(shape)) nrow <- shape[[1]]
+      if (is.null(nrow)) stop("If $X, $obs and $var are NULL, shape should be set to the dimensions of the AnnData.")
+
+      # check ncol size
+      ncol <- ncol(X)
+      if (is.null(ncol)) ncol <- nrow(var)
+      if (is.null(ncol) && !is.null(shape)) ncol <- shape[[2]]
+      if (is.null(ncol)) stop("If $X, $obs and $var are NULL, shape should be set to the dimensions of the AnnData.")
+
+      # check for obs names
+      obs_names <- rownames(X)
+      if (is.null(obs_names)) obs_names <- rownames(obs)
+      if (is.null(obs_names)) obs_names <- as.character(seq_len(nrow))
+
+      # check for var names
+      var_names <- colnames(X)
+      if (is.null(var_names)) var_names <- rownames(var)
+      if (is.null(var_names)) var_names <- as.character(seq_len(ncol))
+
+      # construct obs if it doesn't exist
+      if (is.null(obs)) {
+        obs <- data.frame(row.names = obs_names)
+      }
+      if (!is.data.frame(obs)) stop("$obs should be a data frame.")
+      if (is.null(rownames(obs))) {
+        rownames(obs) <- obs_names
+      }
+
+      # construct var if it doesn't exist
+      if (is.null(var)) {
+        var <- data.frame(row.names = var_names)
+      }
+      if (!is.data.frame(var)) stop("$var should be a data frame.")
+      if (is.null(rownames(var))) {
+        rownames(var) <- var_names
+      }
+
+      # check matrix
+      if (!is.null(X)) {
+        if (nrow(X) != nrow(obs)) stop("$obs should have the same number of rows as $X.")
+        if (ncol(X) != nrow(var)) stop("$obs should have the same number of rows as $X.")
+      }
+
       private$.X <- X
       private$.obs <- obs
       private$.var <- var
