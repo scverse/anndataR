ab38ad31a681d1b0a640ed02b56966dc0435315c
Robrecht Cannoodt <rcannood@gmail.com>
2023-04-28 09:55:06 +0200

    fix issues with to_Seurat conversion

--- R/to_Seurat.R
+++ R/to_Seurat.R
@@ -2,6 +2,8 @@
 #' 
 #' @param obj An AnnData object
 #' 
+#' @importFrom Matrix t
+#' 
 #' @export
 #' @examples
 #' ad <- InMemoryAnnData$new(
@@ -13,24 +15,42 @@
 #' )
 #' to_Seurat(obj)
 to_Seurat <- function(obj) {
-  requireNamespace("Seurat")
+  requireNamespace("SeuratObject")
 
   stopifnot(inherits(obj, "AbstractAnnData"))
 
+  # translate var_names
+  var_names_ <- .toseurat_check_obsvar_names(obj$var_names, "var_names")
+  obs_names_ <- .toseurat_check_obsvar_names(obj$obs_names, "obs_names")
+
   # translate var
   var_ <- obj$var
-  rownames(var_) <- obj$var_names
+  rownames(var_) <- var_names_
 
   # translate obs
   obs_ <- obj$obs
-  rownames(obs_) <- obj$obs_names
+  rownames(obs_) <- obs_names_
+
+  # translate X
+  X_ <- Matrix::t(obj$X)
+  dimnames(X_) <- list(var_names_, obs_names_)
 
   # create assay object
-  X_assay <- Seurat::CreateAssayObject(counts = obj$X)
-  X_assay <- Seurat::AddMetaData(X_assay, metadata = var_)
+  X_assay <- SeuratObject::CreateAssayObject(counts = X_)
+  X_assay <- SeuratObject::AddMetaData(X_assay, metadata = var_)
 
   # create seurat object
-  seurat_obj <- Seurat::CreateSeuratObject(X_assay, meta.data = obs_)
+  seurat_obj <- SeuratObject::CreateSeuratObject(X_assay, meta.data = obs_)
   
   seurat_obj
+}
+
+.toseurat_check_obsvar_names <- function(names, label) {
+  if (any(grepl("_", names))) {
+    # mimic seurat behaviour
+    warning(label, " cannot have underscores ('_') when converting to Seurat, replacing with dashes ('-')")
+    names <- gsub("_", "-", names)
+  }
+
+  names
 }
