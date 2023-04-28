6869c5a2bc5a93c45a85a890a107f214aa2aebc3
Martin Morgan <Martin.Morgan@RoswellPark.org>
2023-04-28 10:51:08 +0200

    update SingleCellExperiment unit tests and man pages

--- tests/testthat/test-SingleCellExperiment.R
+++ tests/testthat/test-SingleCellExperiment.R
@@ -1,8 +1,8 @@
 test_that("AnnData_to_SingleCellExperiment() works", {
     ad <- InMemoryAnnData$new(
         X = matrix(1:5, 3L, 5L),
-        obs = data.frame(cell = 1:3, row.names = LETTERS[1:3]),
-        var = data.frame(gene = 1:5, row.names = letters[1:5])
+        obs = data.frame(cell = 1:3),
+        var = data.frame(gene = 1:5)
     )
 
     expect_no_error(sce <- to_SingleCellExperiment(ad))
@@ -11,8 +11,12 @@ test_that("AnnData_to_SingleCellExperiment() works", {
 
     ad_dimnames <- list(rownames(ad$var), rownames(ad$obs))
     expect_identical(dimnames(sce), ad_dimnames)
-    expect_identical(as.data.frame(SummarizedExperiment::rowData(sce)), ad$var)
-    expect_identical(as.data.frame(SummarizedExperiment::colData(sce)), ad$obs)
+    df <- as.data.frame(SummarizedExperiment::rowData(sce))
+    rownames(df) <- NULL
+    expect_identical(df, ad$var)
+    df <- as.data.frame(SummarizedExperiment::colData(sce))
+    rownames(df) <- NULL
+    expect_identical(df, ad$obs)
 
     expect_identical(
         SummarizedExperiment::assay(sce, withDimnames = FALSE),
