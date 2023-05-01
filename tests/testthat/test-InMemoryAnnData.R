34283da6ec3745e051727f54fb01a9ce14bf5b3b
Martin Morgan <Martin.Morgan@RoswellPark.org>
2023-05-01 15:42:02 -0400

    implement and use layers_keys()
    - use in to_SummarizedExperiment
    - enhance unit tests

--- tests/testthat/test-InMemoryAnnData.R
+++ tests/testthat/test-InMemoryAnnData.R
@@ -20,6 +20,10 @@ var <- data.frame(
 # construct dummy var names
 var_names <- paste0("gene", seq_len(20))
 
+layers = list(
+  X2 = X * 2,
+  X3 = X * 3
+)
 
 # GETTERS ----------------------------------------------------------------
 test_that("create inmemory anndata", {
@@ -141,31 +145,38 @@ test_that("'layers' works", {
   var <- data.frame(y = 1:5)
   layers_test(obs, var, NULL)
   layers_test(obs, var, list(A = matrix(0, 3, 5)))
-  layers_test(obs, var, list(A = matrix(0, 3, 5), B = matrix(0, 3, 5)))
+  layers_test(obs, var, list(A = matrix(0, 3, 5), B = matrix(1, 3, 5)))
 
   ## must be a named list
   expect_error(InMemoryAnnData$new(obs = obs, var = var, layers = list()))
   layers <- list(matrix(0, 3, 5))
   expect_error(InMemoryAnnData$new(obs = obs, var = var, layers = layers))
   ## non-trivial names
-  layers <- list(A = matrix(0, 3, 5), matrix(0, 3, 5))
+  layers <- list(A = matrix(0, 3, 5), matrix(1, 3, 5))
   expect_error(InMemoryAnnData$new(obs = obs, var = var, layers = layers))
   ## matching dimensions
   layers <- list(A = matrix(0, 0, 0))
   expect_error(InMemoryAnnData$new(obs = obs, var = var, layers = layers))
-  layers <- list(A = matrix(0, 3, 5), B = matrix(0, 5, 3))
+  layers <- list(A = matrix(0, 3, 5), B = matrix(1, 5, 3))
   expect_error(InMemoryAnnData$new(obs = obs, var = var, layers = layers))
 })
 
 test_that("*_keys() works", {
   obs <- var <- data.frame()
   ad <- InMemoryAnnData$new(obs = obs, var = var)
+  expect_identical(ad$layers_keys(), NULL)
   expect_identical(ad$obs_keys(), character())
   expect_identical(ad$var_keys(), character())
 
+  layers <- setNames(list(), character())
+  ad <- InMemoryAnnData$new(obs = obs, var = var, layers = layers)
+  expect_identical(ad$layers_keys(), character())
+
+  layers <- list(A = matrix(0, 3, 5), B = matrix(1, 3, 5))
   obs <- data.frame(x = 1:3)
   var <- data.frame(y = 1:5)
-  ad <- InMemoryAnnData$new(obs = obs, var = var)
+  ad <- InMemoryAnnData$new(obs = obs, var = var, layers = layers)
+  expect_identical(ad$layers_keys(), c("A", "B"))
   expect_identical(ad$obs_keys(), "x")
   expect_identical(ad$var_keys(), "y")
 })
@@ -303,15 +314,24 @@ test_that("write to layers", {
     var = var,
     obs_names = obs_names,
     var_names = var_names,
-    layers = list(
-      X2 = X * 2,
-      X3 = X * 3
-    )
+    layers = layers
   )
 
+  ## element assignment
   ad$layers[["X2"]] <- X + 2
   expect_equal(ad$layers[["X2"]], X + 2)
 
   ad$layers[["X4"]] <- X + 4
   expect_equal(ad$layers[["X4"]], X + 4)
+
+  ## list assignment
+  ad$layers <- rev(layers)
+  expect_equal(ad$layers, rev(layers))
+
+  ## remove
+  ad$layers <- NULL
+  expect_true(is.null(ad$layers))
+  ## add to NULL
+  ad$layers <- layers
+  expect_identical(ad$layers, layers)
 })
