14e98d22ab7bf051c1294b24ed6f53d51d7bc747
Robrecht Cannoodt <rcannood@gmail.com>
2023-05-04 01:24:22 +0200

    add more comments

--- R/SingleCellExperiment.R
+++ R/SingleCellExperiment.R
@@ -30,12 +30,15 @@ to_SingleCellExperiment <- function(object) { # nolint
     inherits(object, "AbstractAnnData")
   )
 
+  # trackstatus: class=SingleCellExperiment, feature=get_X, status=done
   ## FIXME: name of 'X' from metadata[["X_name"]]
   x_name <- "X"
   assay_names <- as.character(c(
     if (!is.null(object$X)) x_name,
     object$layers_keys()
   ))
+
+  # trackstatus: class=SingleCellExperiment, feature=get_layers, status=done
   assays <- vector("list", length(assay_names))
   names(assays) <- assay_names
   for (assay in assay_names) {
@@ -48,14 +51,26 @@ to_SingleCellExperiment <- function(object) { # nolint
     assays[[assay]] <- Matrix::t(value)
   }
 
+  # construct colData
+  # trackstatus: class=SingleCellExperiment, feature=get_obs, status=done
+  # trackstatus: class=SingleCellExperiment, feature=get_obs_names, status=done
+  colData <- S4Vectors::DataFrame(
+    object$obs, row.names = object$obs_names
+  )
+
+  # construct rowData
+  # trackstatus: class=SingleCellExperiment, feature=get_var, status=done
+  # trackstatus: class=SingleCellExperiment, feature=get_var_names, status=done
+  rowData <- S4Vectors::DataFrame(
+    object$var, row.names = object$var_names
+  )
+
+  # construct output object
   sce <- SingleCellExperiment::SingleCellExperiment(
     assays = assays,
-    colData = S4Vectors::DataFrame(
-      object$obs, row.names = object$obs_names
-    ),
-    rowData = S4Vectors::DataFrame(
-      object$var, row.names = object$var_names
-    ),
+    
+    colData = colData,
+    rowData = rowData,
     metadata = list(),
     ## FIXME: assign object$uns to metadata
     checkDimnames = TRUE
