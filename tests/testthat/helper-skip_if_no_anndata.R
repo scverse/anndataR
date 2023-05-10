e108fec5138a709e5cc3fce556e746b09cb8cdea
Luke Zappia <lazappi@users.noreply.github.com>
2023-05-10 14:47:13 +0200

    Make skip_if_no_anndata() message clearer
    Now says it is looking for Python anndata

--- tests/testthat/helper-skip_if_no_anndata.R
+++ tests/testthat/helper-skip_if_no_anndata.R
@@ -1,8 +1,9 @@
-# helper function to skip tests if we don't have the 'anndata' module
+# helper function to skip tests if we don't have the Python 'anndata' module
 skip_if_no_anndata <- function() {
+  testthat::skip_if_not_installed("reticulate")
   requireNamespace("reticulate")
   testthat::skip_if_not(
     reticulate::py_module_available("anndata"),
-    message = "anndata not available for testing"
+    message = "Python anndata module not available for testing"
   )
 }
