66ad1043334a748dbbddf0f4f19f6324d8e81653
Luke Zappia <lazappi@users.noreply.github.com>
2023-07-06 11:19:29 +0200

    Add read_h5ad function

--- R/HDF5-read.R
+++ R/HDF5-read.R
@@ -422,3 +422,32 @@ read_h5ad_collection <- function(file, name, column_order) {
   }
   columns
 }
+
+#' Read H5AD
+#'
+#' Read data from a H5AD file
+#'
+#' @param path Path to the H5AD file to read
+#' @param to The type of object to return, either "SingleCellExperiment" or
+#' "Seurat"
+#'
+#' @return The object specified by `to`
+#' @export
+#'
+#' @examples
+#' h5ad_file <- system.file("extdata", "example.h5ad", package = "anndataR")
+#' # Read the H5AD as a SingleCellExperiment object
+#' sce <- read_h5ad(h5ad_file, to = "SingleCellExperiment")
+#' # Read the H5AD as a Seurat object
+#' seurat <- read_h5ad(h5ad_file, to = "Seurat")
+read_h5ad <- function(path, to = c("SingleCellExperiment", "Seurat")) {
+  
+  to <- match.arg(to)
+  
+  adata <- HDF5AnnData$new(path)
+  
+  switch (to,
+    "SingleCellExperiment" = to_SingleCellExperiment(adata),
+    "Seurat" = to_Seurat(adata)
+  )
+}
