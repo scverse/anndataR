41e7eb0cbbb98d264a47334ea42ae06fb9430b97
Isaac Virshup <ivirshup@gmail.com>
2023-09-19 15:17:03 +0200

    Anndata obsm varm3 (#125)
    * Start obsm/ varm
    
    * InMemory + tests
    
    * Add valdation
    
    * Start roundtrip
    
    * Start validation
    
    * Style
    
    * Get tests running
    
    * Remove some TODOs
    
    * obsp/ varp
    
    * Don't add row/ col names in validation (still needs tests)
    
    * tests obsp, varp validation
    
    * Consolidate layer validation with aligned mapping validation
    
    * Shorten lines
    
    * Fix repeated trackstatus item
    
    * add a helpful error message when duplicate trackstatus are found
    
    * add docs, change trackstatus from wip to done
    
    * try line length fix
    
    * Manually 😭 fix linting issues
    
    * Bump build
    
    * import testthat?
    
    ---------
    
    Co-authored-by: Robrecht Cannoodt <rcannood@gmail.com>

--- tests/testthat/test-HDF5AnnData.R
+++ tests/testthat/test-HDF5AnnData.R
@@ -27,6 +27,63 @@ test_that("reading layers works", {
   )
 })
 
+test_that("reading obsm works", {
+  obsm <- adata$obsm
+  expect_true(is.list(obsm), "list")
+  expect_equal(
+    names(obsm),
+    c("X_pca", "X_umap")
+  )
+})
+
+test_that("reading varm works", {
+  varm <- adata$varm
+  expect_true(is.list(varm), "list")
+  expect_equal(
+    names(varm),
+    c("PCs")
+  )
+})
+
+test_that("obsm/ varm validation", {
+  N_OBS <- 5
+  N_VAR <- 3
+
+  mtx <- matrix(
+    0,
+    N_OBS,
+    N_VAR
+  )
+
+  adata <- AnnData(
+    X = mtx,
+    obs_names = as.character(1:N_OBS),
+    var_names = as.character(1:N_VAR)
+  )
+
+  adata$obsm <- list(PCA = matrix(0, N_OBS, 4))
+  adata$varm <- list(PCs = matrix(0, N_VAR, 4))
+
+  expect_error(adata$obsm <- list(PCA = matrix(0, 4, 4)))
+  expect_error(adata$varm <- list(PCs = matrix(0, 4, 4)))
+})
+
+test_that("obsp/ varp validation", {
+  N_OBS <- 5
+  N_VAR <- 3
+
+  adata <- AnnData(
+    obs_names = as.character(1:N_OBS),
+    var_names = as.character(1:N_VAR)
+  )
+
+  adata$obsp <- list(graph1 = matrix(0, N_OBS, N_OBS))
+  adata$varp <- list(graph1 = matrix(0, N_VAR, N_VAR))
+
+  expect_error(adata$obsp <- list(graph1 = matrix(0, 4, 4)))
+  expect_error(adata$varp <- list(graph1 = matrix(0, 4, 4)))
+})
+
 # trackstatus: class=HDF5AnnData, feature=test_get_obs, status=done
 test_that("reading obs works", {
   obs <- adata$obs
