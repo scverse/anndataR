ebde5804c20a38b18d18f0ee51c9578b789fa83c
Robrecht Cannoodt <rcannood@gmail.com>
2023-11-30 11:15:35 +0100

    refactor roundtrip tests

--- tests/testthat/test-roundtrip.R
+++ tests/testthat/test-roundtrip.R
@@ -1,38 +1,37 @@
 library(testthat)
+library(Matrix)
 
 h5ad_file <- tempfile(pattern = "hdf5_write_", fileext = ".h5ad")
 
 base_file <- system.file("extdata", "example.h5ad", package = "anndataR")
 
-
 gen_adata <- function(type) {
-  library(Matrix)
   N_OBS <- 10
   N_VAR <- 15
-  obs_names <- paste0("obs_", 1:N_OBS)
-  var_names <- paste0("var_", 1:N_VAR)
+  obs_names <- paste0("obs_", seq_len(N_OBS))
+  var_names <- paste0("var_", seq_len(N_VAR))
   adata <- AnnData(
     X = rsparsematrix(N_OBS, N_VAR, 0.1),
     obs_names = obs_names,
     var_names = var_names,
     layers = list(
-      dense = matrix(1:15, N_OBS, N_VAR),
+      dense = matrix(seq_len(N_OBS * N_VAR), N_OBS, N_VAR),
       sparse = rsparsematrix(N_OBS, N_VAR, 0.1)
     ),
     obsm = list(
-      dense = matrix(1:15, N_OBS, 5),
+      dense = matrix(seq_len(N_OBS * 5), N_OBS, 5),
       sparse = rsparsematrix(N_OBS, 5, 0.1)
     ),
     varm = list(
-      dense = matrix(1:15, N_VAR, 5),
+      dense = matrix(seq_len(N_VAR * 5), N_VAR, 5),
       sparse = rsparsematrix(N_VAR, 5, 0.1)
     ),
     obsp = list(
-      dense = matrix(1:15, N_OBS, N_OBS),
+      dense = matrix(seq_len(N_OBS * N_OBS), N_OBS, N_OBS),
       sparse = rsparsematrix(N_OBS, N_OBS, 0.1)
     ),
     varp = list(
-      dense = matrix(1:15, N_VAR, N_VAR),
+      dense = matrix(seq_len(N_VAR * N_VAR), N_VAR, N_VAR),
       sparse = rsparsematrix(N_VAR, N_VAR, 0.1)
     )
   )
@@ -55,13 +54,10 @@ check_round_trip <- function(expected, type) {
   expect_equal(actual, expected)
 }
 
-check_round_trip_example <- function(type) {
-  check_round_trip(read_h5ad(base_file, to = type), type)
-}
-
 for (typ in c("HDF5AnnData", "InMemoryAnnData")) {
   test_that(paste("round trip w/ example data for", typ), {
-    check_round_trip_example(typ)
+    adata <- read_h5ad(base_file, to = typ)
+    check_round_trip(adata, typ)
   })
   test_that(paste("round trip w/ generated data for", typ), {
     adata <- gen_adata(typ)
