b9b9685b9ea545d2b9cac2e4003516ebbb911780
Louise Deconinck <louise.deconinck@gmail.com>
2024-07-01 20:43:14 +0200

    Eliminate most randomness, check what happens when using np.nan, pd.NA or None

--- inst/scripts/generate_vector.py
+++ inst/scripts/generate_vector.py
@@ -2,30 +2,49 @@ import random as rd
 import pandas as pd
 import numpy as np
 
-# TODO: I don't like the randomness here, find a different way
-# TODO: check what numpy & pandas does with np.nan and pd.NA and None? Should these be included as well?
-# TODO: check as what these values are written to the h5ad files
-vector_generators = {
-    "categorical": lambda n: pd.array(rd.choices(["Value1", "Value2", "Value3"], k=n), dtype="category"),
-    "categorical_ordered": lambda n: pd.array(rd.choices(["Value1", "Value2", "Value3"], k=n), 
-                                              dtype=pd.CategoricalDtype(["Value1", "Value2", "Value3"], ordered=True)),
-    "categorical_missing_values": lambda n: pd.array(rd.choices(["Value1", "Value2", "Value3", np.nan], k=n), dtype="category"), 
-    "categorical_ordered_missing_values": lambda n: pd.array(rd.choices(["Value1", "Value2", "Value3", np.nan], k=n), 
-                                                             dtype=pd.CategoricalDtype(["Value1", "Value2", "Value3"], ordered=True)),
-    
-    "string_array": lambda n: [f"value" for _ in range(n)],
 
-    # should we also check a 1d sparse array? We should probably leave it for the matrix generation
+def nullable_integer_array(n):
+    nullable_array = [1 for _ in range(n)]
+    # np.nan, pd.NA and None should all end up as null values, masked in the h5ad file
+    nullable_array[0] = pd.NA
+    nullable_array[1] = np.nan
+    nullable_array[2] = None
+    return pd.array(nullable_array, dtype="Int64")
+
+def nullable_boolean_array(n):
+    nullable_array = [rd.choices([True, False], k=n)]
+    # np.nan, pd.NA and None should all end up as null values, masked in the h5ad file
+    nullable_array[0] = pd.NA
+    nullable_array[1] = np.nan
+    nullable_array[2] = None
+    return pd.array(nullable_array, dtype="boolean")
+
+def missing_values_categorical(n, ordered = True):
+    missing_values = pd.Categorical([["Value1", "Value2", "Value3"][i % 3] for i in range(n)], 
+                              categories = ["Value1", "Value2", "Value3"],
+                              ordered=ordered)
+    # They should all end up as code -1 in the h5ad file
+    missing_values[0] = np.nan
+    missing_values[1] = pd.NA
+    missing_values[2] = None
+    return missing_values
+
+vector_generators = {
+    "categorical": lambda n: pd.Categorical([["Value1", "Value2", "Value3"][i % 2] for i in range(n)]),
+    "categorical_ordered": lambda n: pd.Categorical([["Value1", "Value2", "Value3"][i % 2] for i in range(n)], ordered=True),
+    "categorical_missing_values": lambda n: missing_values_categorical(n, ordered=False), 
+    "categorical_ordered_missing_values": lambda n: missing_values_categorical(n, ordered=True),
+
+    "string_array": lambda n: [f"value_{i}" for i in range(n)],
+
+    # should we also check a 1d sparse array? We should probably leave it for the matrix generation?
     "dense_array": lambda n: [rd.random() for _ in range(n)],
 
     "integer_array": lambda n: [1 for _ in range(n)],
-    "nullable_integer_array": lambda n: pd.array(rd.choices(["Value1", "Value2", np.nan], k=n), dtype="Int64"),
+    "nullable_integer_array": nullable_integer_array,
 
     "boolean_array": lambda n: [rd.choices([True, False], k=n)],
-    # should we also test np.nan in a boolean array (which should always evaluate to False)?
-    "nullable_boolean_array": lambda n: pd.array(rd.choices(["Value1", "Value2", pd.NA], k=n), dtype="boolean"),
-    "boolean_array_nan": lambda n: pd.array(rd.choices([True, False, np.nan], k=n), dtype="boolean"),
-
+    "nullable_boolean_array": nullable_boolean_array,
 }
 
 def generate_vector(vector_type, n):
