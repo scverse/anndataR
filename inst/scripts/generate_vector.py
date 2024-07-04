11a0a671ec4ec0beee674126d29507f42b0ea71a
Louise Deconinck <louise.deconinck@gmail.com>
2024-07-04 22:10:46 +0200

    generate dict and start of dataset

--- inst/scripts/generate_vector.py
+++ inst/scripts/generate_vector.py
@@ -4,29 +4,32 @@ import numpy as np
 
 
 def nullable_integer_array(n):
+    assert n > 0, "an integer array must be at least one value"
     nullable_array = [1 for _ in range(n)]
     # np.nan, pd.NA and None should all end up as null values, masked in the h5ad file
-    nullable_array[0] = pd.NA
-    nullable_array[1] = np.nan
-    nullable_array[2] = None
+    nullable_array[0] = np.nan
+    if n > 1: nullable_array[1] = pd.NA
+    if n > 2: nullable_array[2] = None
     return pd.array(nullable_array, dtype="Int64")
 
 def nullable_boolean_array(n):
+    assert n > 0, "a boolean array must be at least one value"
     nullable_array = rd.choices([True, False], k=n)
     # np.nan, pd.NA and None should all end up as null values, masked in the h5ad file
     nullable_array[0] = pd.NA
-    nullable_array[1] = np.nan
-    nullable_array[2] = None
+    if n > 1: nullable_array[1] = np.nan
+    if n > 2: nullable_array[2] = None
     return pd.array(nullable_array, dtype="boolean")
 
 def missing_values_categorical(n, ordered = True):
+    assert n > 0, "a categorical must be at least one value"
     missing_values = pd.Categorical([["Value1", "Value2", "Value3"][i % 3] for i in range(n)], 
                               categories = ["Value1", "Value2", "Value3"],
                               ordered=ordered)
     # They should all end up as code -1 in the h5ad file
     missing_values[0] = np.nan
-    missing_values[1] = pd.NA
-    missing_values[2] = None
+    if n > 1: missing_values[1] = pd.NA
+    if n > 2: missing_values[2] = None
     return missing_values
 
 vector_generators = {
