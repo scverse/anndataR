11850b240a41a5d1f0df4c3110ca71941794e43b
Louise Deconinck <louise.deconinck@gmail.com>
2024-07-04 20:25:20 +0200

    documentation

--- inst/scripts/generate_dataframe.py
+++ inst/scripts/generate_dataframe.py
@@ -1,7 +1,20 @@
 import pandas as pd
 from generate_vector import vector_generators
 
-def generate_dataframe(n_rows, types = None):
+def generate_dataframe(n_rows, types=None):
+    """
+    Generate a pandas DataFrame with specified number of rows and column types.
+
+    Parameters:
+        n_rows (int): The number of rows in the DataFrame.
+        types (list, optional): A list of column types to include in the DataFrame.
+                                Choose from the list of vector_generators keys.
+                                If not provided, all available column types will be included.
+
+    Returns:
+        pandas.DataFrame: The generated DataFrame.
+
+    """
     if types is None:
         types = list(vector_generators.keys())
 
