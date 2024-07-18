92ee0a4f9c2f216e774afdadaba54d1b4878c180
Robrecht Cannoodt <rcannood@gmail.com>
2024-07-18 21:36:16 +0200

    add test (this seems to have been fixed at some point)

--- tests/testthat/test-roundtrip-obsvar.R
+++ tests/testthat/test-roundtrip-obsvar.R
@@ -8,8 +8,6 @@ test_names <- names(data$obs)
 # TODO: re-enable tests
 test_names <- test_names[!grepl("_with_nas", test_names)]
 # TODO: re-enable tests
-test_names <- test_names[!test_names %in% c("factor", "factor_ordered")]
-# TODO: re-enable tests
 test_names <- test_names[test_names != "logical"]
 
 for (name in test_names) {
