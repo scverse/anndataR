3c60c3ebb7e5a556f15dc787fe26d6f0e25176c9
Louise Deconinck <louise.deconinck@gmail.com>
2024-10-04 11:11:18 +0200

    lintr

--- tests/testthat/test-h5diff.R
+++ tests/testthat/test-h5diff.R
@@ -8,8 +8,8 @@ testthat::skip_if_not(
 
 test_that("h5diff", {
   requireNamespace("processx")
-  
-  data_R <- generate_dataset(10L, 20L, format = "AnnData")
+
+  data_r <- generate_dataset(10L, 20L, format = "AnnData")
 
   da <- reticulate::import("dummy_anndata")
   data_python <- da$generate_dataset(10L, 20L)
@@ -17,11 +17,10 @@ test_that("h5diff", {
   h5ad_file1 <- tempfile(pattern = "hdf5_write_R_", fileext = ".h5ad")
   h5ad_file2 <- tempfile(pattern = "hdf5_write_py_", fileext = ".h5ad")
 
-  write_h5ad(data_R, h5ad_file1)
+  write_h5ad(data_r, h5ad_file1)
   data_python$write_h5ad(h5ad_file2)
   res <- processx::run("h5diff", c("-v", h5ad_file1, h5ad_file2), error_on_status = FALSE)
 
   expect_equal(res$status, 0, info = res$stdout)
 
 })
-
