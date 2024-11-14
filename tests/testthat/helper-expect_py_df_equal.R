0e65cbd62bc9f9493ea6a04aeaabc75424877257
Robrecht Cannoodt <rcannood@gmail.com>
2024-11-14 11:16:06 +0100

    fix linting issues

--- tests/testthat/helper-expect_py_df_equal.R
+++ tests/testthat/helper-expect_py_df_equal.R
@@ -1,7 +1,13 @@
 expect_py_df_equal <- function(a, b) {
+  requireNamespace("testthat")
+  requireNamespace("reticulate")
+
+  bi <- reticulate::import_builtins()
   pd <- reticulate::import("pandas")
 
-  expect_null(
+  testthat::expect_equal(bi$type(a), bi$type(b)) # does this always work?
+
+  testthat::expect_null(
     pd$testing$assert_frame_equal(
       a,
       b,
--- tests/testthat/helper-expect_py_matrix_equal.R
+++ tests/testthat/helper-expect_py_matrix_equal.R
@@ -1,13 +1,16 @@
 expect_py_matrix_equal <- function(a, b, rtol = 1e-6, atol = 1e-6) {
+  requireNamespace("testthat")
+  requireNamespace("reticulate")
+
   bi <- reticulate::import_builtins()
   np <- reticulate::import("numpy")
   scipy <- reticulate::import("scipy")
 
-  expect_equal(bi$type(a), bi$type(b)) # does this always work?
+  testthat::expect_equal(bi$type(a), bi$type(b)) # does this always work?
 
-  expect_equal(a$dtype, b$dtype)
+  testthat::expect_equal(a$dtype, b$dtype)
 
-  expect_equal(reticulate::py_to_r(a$shape), reticulate::py_to_r(b$shape))
+  testthat::expect_equal(reticulate::py_to_r(a$shape), reticulate::py_to_r(b$shape))
 
   a_dense <-
     if (scipy$sparse$issparse(a)) {
@@ -22,7 +25,7 @@ expect_py_matrix_equal <- function(a, b, rtol = 1e-6, atol = 1e-6) {
       b
     }
 
-  expect_null(
+  testthat::expect_null(
     np$testing$assert_allclose(a_dense, b_dense, rtol = rtol, atol = atol)
   )
 }
