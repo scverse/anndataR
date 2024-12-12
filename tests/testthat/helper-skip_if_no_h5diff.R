4a4790b54e9b832c62117ce4b0fa10768be987d3
LouiseDck <louise.deconinck@gmail.com>
2024-12-12 21:41:59 +0100

    lintr

--- tests/testthat/helper-skip_if_no_h5diff.R
+++ tests/testthat/helper-skip_if_no_h5diff.R
@@ -1,7 +1,7 @@
 # helper function to skip tests if h5diff is not available
-skip_if_no_h5diff <- function(){
-    testthat::skip_if(
-        !system("which h5diff", ignore.stdout = TRUE),
-        message = "h5diff not available for testing"
-    )
+skip_if_no_h5diff <- function() {
+  testthat::skip_if(
+    !system("which h5diff", ignore.stdout = TRUE),
+    message = "h5diff not available for testing"
+  )
 }
