06000093bdb41666efc050729c7777a15792abc5
LouiseDck <louise.deconinck@gmail.com>
2024-12-12 22:49:50 +0100

    Fix system2 call

--- tests/testthat/helper-skip_if_no_h5diff.R
+++ tests/testthat/helper-skip_if_no_h5diff.R
@@ -1,11 +1,7 @@
 # helper function to skip tests if h5diff is not available
 skip_if_no_h5diff <- function() {
-  testthat::skip_if(
-    tryCatch({
-      system2("which h5diff")
-      FALSE
-    }, error = function(e) TRUE
-    ),
-    message = "h5diff not available for testing"
-  )
+  testthat::skip_if_not({
+    s <- system2(command = "which", args = "h5diff", stdout = TRUE, stderr = TRUE)
+    is.null(attr(s, "status"))
+  }, message = "h5diff not available for testing")
 }
