82bf2a0de39ea8114c0b6b5189cf650a593de161
Robrecht Cannoodt <rcannood@gmail.com>
2024-12-19 20:07:12 +0100

    Add uns roundtrip tests (#210)
    * add uns_keys function
    
    * add extra check to unit tests
    
    * update the uns rountrip tests
    
    * add uns-nested roundtrip test

--- tests/testthat/test-roundtrip-layers.R
+++ tests/testthat/test-roundtrip-layers.R
@@ -99,6 +99,12 @@ for (name in test_names) {
     # read from file
     adata_py2 <- ad$read_h5ad(file_r)
 
+    # expect name is one of the keys
+    expect_contains(
+      bi$list(adata_py2$layers$keys()),
+      name
+    )
+
     # expect that the objects are the same
     expect_equal_py(
       py_get_item(adata_py2$layers, name),
