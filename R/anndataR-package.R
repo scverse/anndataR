8b18ae91a337e7c3b78321e7b5f6759f5a463946
Luke Zappia <luke@data-intuitive.com>
2025-05-05 08:32:36 +0200

    Fix R6 import NOTE

--- R/anndataR-package.R
+++ R/anndataR-package.R
@@ -6,6 +6,7 @@
 #' @importFrom lifecycle deprecated
 #' @importFrom methods as new
 #' @importFrom purrr map_lgl map_dfr
+#' @importFrom R6 R6Class
 #' @importFrom rlang `%||%`
 #' @importFrom stats setNames
 ## usethis namespace: end
