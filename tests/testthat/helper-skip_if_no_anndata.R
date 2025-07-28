3dc981a5739ba6059ddf767db9cc50e0e9ca1563
Luke Zappia <luke@data-intuitive.com>
2025-07-28 14:23:36 +0200

    Pin anndata<=0.11.4 for tests

--- tests/testthat/helper-skip_if_no_anndata.R
+++ tests/testthat/helper-skip_if_no_anndata.R
@@ -4,7 +4,7 @@ skip_if_no_anndata <- function() {
   testthat::skip_if_not_installed("reticulate")
   testthat::skip_if_not_installed("anndata")
   requireNamespace("reticulate")
-  reticulate::py_require("anndata")
+  reticulate::py_require("anndata<=0.11.4")
   testthat::skip_if_not(
     reticulate::py_module_available("anndata"),
     message = "Python anndata module not available for testing"
