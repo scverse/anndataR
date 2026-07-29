325cfad32733491875d0538b293d4d54fa5a0e38
Hugo Gruson <git@hugogruson.fr>
2026-07-29 11:12:37 +0200

    Add missing trailing slash to root path

--- R/ZarrAnnData.R
+++ R/ZarrAnnData.R
@@ -563,7 +563,7 @@ ZarrAnnData <- R6::R6Class(
       }
 
       # Create consolidated metadata on the fly, in memory, if it doesn't exist on disk
-      consolidated <- Rarr:::.read_consolidated_metadata(file)$metadata %||%
+      consolidated <- Rarr:::.read_consolidated_metadata(paste0(file, "/"))$metadata %||%
         Rarr::zarr_consolidate_metadata(file, action = "return")
       private$.consolidated_metadata <- list2env(consolidated, hash = TRUE, parent = emptyenv())
 
