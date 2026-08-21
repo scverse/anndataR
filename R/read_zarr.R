55e290a64ff9bdd4e4c9c603d7ae0e64559c2f27
Artur-man <artur-man@hotmail.com>
2026-08-21 18:51:47 +0200

    more docs

--- R/read_zarr.R
+++ R/read_zarr.R
@@ -19,7 +19,7 @@
 #'   * `r+` opens an existing file for read/write.
 #'   * `w` creates a file, truncating any existing ones.
 #'   * `w-`/`x` are synonyms, creating a file and failing if it already exists.
-#' @param backed Whether to read the H5AD file in backed mode, returning an
+#' @param backed Whether to read the Zarr store in backed mode, returning an
 #'  object containing [DelayedArray::DelayedMatrix] matrices. Which slots are
 #'  backed depends on the value of `as`.
 #' @param ... Extra arguments provided to the `as_*` conversion function for the
